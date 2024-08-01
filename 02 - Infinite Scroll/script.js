aa2e182ccb5cee5d4ec23c5a3ff64b818d5fb9ea
Gustav-i <112987943+Gustav-i@users.noreply.github.com>
2024-08-01 17:29:52 +0200

    update

--- 02 - Infinite Scroll/script.js
+++ 02 - Infinite Scroll/script.js
@@ -1,20 +1,94 @@
-const count = 10
+const imageContainer = document.getElementById('img-container')
+const loader = document.getElementById('loader')
+
+let ready = false
+let imagesLoaded = 0
+let totalImages = 0
+let photosArray = []
+
+const count = 5
 const API_KEY = '-Fnu3GWCV_TucIkxRjr91T7B8TbiZbTBxETi8jCFbos'
+const filter = "landscape"
 
-const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&count=${count}`
+const API_URL = `https://api.unsplash.com/photos/random/?client_id=${API_KEY}&orientation=${filter}&count=${count}`
 
+
+// Check if all images were loaded
+function imageLoaded() 
+{
+  imagesLoaded++
+
+  if (imagesLoaded === totalImages) 
+  {
+    ready = true
+    loader.hidden = true
+  }
+}
+
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) 
+{
+  for (const key in attributes) 
+  {
+    element.setAttribute(key, attributes[key])
+  }
+}
+
+// Create Elements For Links & Photos, Add to DOM
+function displayPhotos() 
+{
+  imagesLoaded = 0
+  totalImages = photosArray.length
+  // Run function for each object in photosArray
+  photosArray.forEach((photo) => 
+  {
+    // Create <a> to link to full photo
+    const item = document.createElement('a')
+    setAttributes(item, 
+    {
+      href: photo.links.html,
+      target: '_blank',
+    })
+    // Create <img> for photo
+    const img = document.createElement('img')
+    
+    setAttributes(img, 
+    {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    })
+    // Event Listener, check when each is finished loading
+    img.addEventListener('load', imageLoaded)
+    // Put <img> inside <a>, then put both inside imageContainer Element
+    item.appendChild(img)
+    imageContainer.appendChild(item)
+  })
+}
+
+// Get photos from Unsplash API
 async function getPhotos() 
 {
   try 
   {
     const response = await fetch(API_URL)
-    const data = await response.json()
-    console.log(data)
-  }
+    photosArray = await response.json()
+    displayPhotos()
+  } 
   catch (error) 
   {
-    console.log(error)
-  }   
+    alert(error)
+  }
 }
 
+// Check to see if scrolling near bottom of page, Load More Photos
+window.addEventListener('scroll', () => 
+{
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 && ready) {
+    ready = false
+    getPhotos()
+  }
+})
+
+// On Load
 getPhotos()
