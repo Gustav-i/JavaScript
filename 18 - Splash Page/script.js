aee85a70d305612df4712da413c672665a73545f
Gustav-i <112987943+Gustav-i@users.noreply.github.com>
2024-08-30 08:20:37 +0200

    update

---
+++ 18 - Splash Page/script.js
@@ -0,0 +1,32 @@
+const { body } = document
+
+
+function changeBackground(number) 
+{
+  let previousBackground
+  
+  if (body.className) 
+  {
+    previousBackground = body.className  
+  }
+
+  body.className = ""
+
+  switch (number) 
+  {
+    case "1":
+      return (previousBackground === "background-1" ? null 
+        : body.classList.add("background-1"))
+
+    case "2":
+      return (previousBackground === "background-2" ? null 
+        : body.classList.add("background-2"))
+
+    case "3":
+      return (previousBackground === "background-3" ? null 
+        : body.classList.add("background-3"))
+        
+    default:
+      break
+  }
+}
